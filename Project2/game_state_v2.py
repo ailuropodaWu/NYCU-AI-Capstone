4dfe189036f852e65698fe7f189110c2c1eeae88
William Liao <williamliao2015@gmail.com>
2024-04-09 13:47:32 +0800

    Remove connected regions part and enable free split

--- Project2/game_state_v2.py
+++ Project2/game_state_v2.py
@@ -27,28 +27,25 @@ class GameState(BaseGameState):
             if move[-1] % 2 == 0: fourNeighbors += 1
             else: eightNeighbors += 1
         sheeps = self.sheep[self.mapStat == id]
-        # boundingBox = get_bbox(self.mapStat == id)
-        # area_part = -(0.5 * (boundingBox[1] - boundingBox[0] + 1) * (boundingBox[3] - boundingBox[2] + 1) / 144) # avoid area too large
-        connected_part = np.max([len(r) for r in findConnected(self, id)]) / self.maxSheep # prefer large connected regions
         score_part = self.scores[id - 1] / (self.maxSheep ** 1.25) # 16 ^ 1.25 = 32
         neighbors_part = (0.7 * fourNeighbors + 0.3 * eightNeighbors) / (4 * len(legalMoves)) if len(legalMoves) else 0 # prefer 4 neighbors over 8 neighbors
         rank_part = 1 - rank / 4 # prefer higher rank
         sheeps_part = -(0.5 * np.max(sheeps) / self.maxSheep) # avoid sheep to be too concentrated
-        return np.dot([connected_part, score_part, neighbors_part, rank_part, sheeps_part], [0.2, 0.2, 0.2, 0.2, 0.2])
+        return np.dot([score_part, neighbors_part, rank_part, sheeps_part], [0.2, 0.2, 0.2, 0.2])
 
-    # def getLegalMoves(self, id):
-    #     legalMoves = []
-    #     for row, col in np.ndindex(self.mapStat.shape):
-    #         # Select cells with more than one sheep
-    #         if self.mapStat[row, col] != id or self.sheep[row, col] <= 1: continue
-    #         for dir_i, dir in enumerate(DIRECTION):
-    #             if dir_i == 4: continue
-    #             if 0 <= row + dir[0] < len(self.mapStat) and \
-    #                 0 <= col + dir[1] < len(self.mapStat[0]) and \
-    #                 self.mapStat[row+dir[0], col+dir[1]] == 0:
-    #                 # only consider half split
-    #                 legalMoves.extend([[(row, col), sheep_num, dir_i + 1] for sheep_num in range(1, int(self.sheep[row, col]))])
-    #     return legalMoves
+    def getLegalMoves(self, id):
+        legalMoves = []
+        for row, col in np.ndindex(self.mapStat.shape):
+            # Select cells with more than one sheep
+            if self.mapStat[row, col] != id or self.sheep[row, col] <= 1: continue
+            for dir_i, dir in enumerate(DIRECTION):
+                if dir_i == 4: continue
+                if 0 <= row + dir[0] < len(self.mapStat) and \
+                    0 <= col + dir[1] < len(self.mapStat[0]) and \
+                    self.mapStat[row+dir[0], col+dir[1]] == 0:
+                    # only consider half split
+                    legalMoves.extend([[(row, col), sheep_num, dir_i + 1] for sheep_num in range(1, int(self.sheep[row, col]))])
+        return legalMoves
 
 class EndGameState(GameState):
     def __init__(self, _mapStat, _sheepStat, _maxSheep, _playerNum=4):
