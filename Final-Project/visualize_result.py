c3ae6550c2fc6d7a9a2c9186e14ac1c1d48673f6
ailuropodaWu <ailuropoda.cs10@nycu.edu.tw>
2024-05-13 14:11:46 +0800

    modify the evaluation metrics & add tf-idf cosine similarity

--- Final-Project/visualize_result.py
+++ Final-Project/visualize_result.py
@@ -1,34 +1,22 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from scipy.stats import pearsonr, spearmanr
 from argparse import ArgumentParser
 
-def visualize(prediction_path):
-    def rmse(truth, pred):
-        return np.sqrt(np.sum(np.square(truth - pred)) / truth.size)
+def evaluation(prediction_path):
+    def rmse(score, pred):
+        return np.sqrt(np.sum(np.square(score - pred)) / score.size)
     df = pd.read_json(prediction_path)
-    truth = df['score']
-    pred = df['similarity']
-    
-    print(f"RMSE: {rmse(truth, pred)}")
-    
-    for ran in range(5):
-        t = truth[(ran <= truth) & (truth <= ran + 1)]
-        p = pred[(ran <= truth) & (truth <= ran + 1)]
-        print(f"[{ran} - {ran + 1}] RMSE: {rmse(t, p): .3f}, NUM: {t.size}")
-    
-    handle = [truth, pred]
-    handle = np.array(handle)
-    handle = handle.transpose()
-    handle.sort(-1)
-    handle = handle.transpose()
-    plt.scatter(handle[0], handle[1])
-    plt.show()
+    score = df['score']
+    cos_sim = df['cos-sim']
+    tfidf_cos = df['tfidf-cos']
+    print(pearsonr(score, cos_sim), spearmanr(score, cos_sim))
+    print(pearsonr(tfidf_cos, cos_sim), spearmanr(tfidf_cos, cos_sim))
     
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--model", type=str, help="Choose the directory of the model")
     args = parser.parse_args()
     model = args.model
-    visualize(f'{model}/prediction.json')
-    
+    evaluation(f'{model}/prediction.json')
