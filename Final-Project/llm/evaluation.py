0fe1e728cfc3ec3f62f3f3945fd073f7dcd92cf3
William Liao <williamliao2015@gmail.com>
2024-06-01 00:09:23 +0800

    Update evaluation.py with tqdm progress bar for dataset iteration

--- Final-Project/llm/evaluation.py
+++ Final-Project/llm/evaluation.py
@@ -1,6 +1,7 @@
 import numpy as np
 from torch.nn.functional import cosine_similarity, cross_entropy
 from torch import Tensor
+from tqdm import tqdm
 from ..dataset.load_eval_data import load_eval_data
 
 
@@ -8,12 +9,9 @@ def evaluation(model):
     truth = []
     pred = []
     dataset = load_eval_data()
-    for _, data in dataset.iterrows():
-        embedding = [model(sentence) for sentence in data.values[:-1]]
-        similarity = [cosine_similarity(embed, embedding[-1]) for embed in embedding[:-1]]
+    for _, data in tqdm(dataset.iterrows(), total=len(dataset)):
+        embeddings = model(data.values[:-1])
+        similarity = [cosine_similarity(embed, embeddings[[-1]]).cpu() for embed in embeddings[:-1]]
         truth.append(data['label'])
         pred.append(np.argmax(similarity))
-        truth = Tensor(truth)
-        pred = Tensor(pred)
-    return cross_entropy(pred, truth)
-
+    return cross_entropy(Tensor(pred), Tensor(truth)).item(), Tensor(pred) == Tensor(truth) / len(truth)
