3883ba6dc027cf6e471a63407d753020e97034b2
William Liao <williamliao2015@gmail.com>
2024-06-01 22:07:19 +0800

    Update SentenceTransformerModel and optimize text-embedding model finetuning

--- Final-Project/llm/sentence_transformer_model.py
+++ Final-Project/llm/sentence_transformer_model.py
@@ -2,8 +2,8 @@ import os
 
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torch.nn.functional as F
+import torch.optim as optim
 import lightning as L
 
 from torch.utils.data import DataLoader
@@ -15,29 +15,35 @@ os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 
 
 class SentenceTransformerModel(L.LightningModule):
-    def __init__(self, batch_size=4, n_ctx=512, n_emb=256, learning_rate=1e-5, temperature=0.05):
+    def __init__(self, batch_size=4, n_ctx=512, n_emb=1024, learning_rate=1e-5, temperature=0.05):
         super(SentenceTransformerModel, self).__init__()
+        self.model_name = "all-MiniLM-L12-v2"
+
         self.batch_size = batch_size
         self.n_ctx = n_ctx
         self.learning_rate = learning_rate
         self.temperature = temperature
-        self.lm = SentenceTransformer("intfloat/e5-large-v2", device=self.device)
-        self.mlp = nn.Sequential(
-            nn.Linear(n_emb, 1024),
+
+        self.embedding_model = SentenceTransformer(self.model_name, device=self.device)
+        self.embedding_model.max_seq_length = self.n_ctx
+        self.embedding_model.requires_grad_(False)
+        self.embedding_model.eval()
+
+        self.seq = nn.Sequential(
+            nn.Linear(384, 2048),
             nn.ReLU(),
-            nn.Linear(1024, 512),
+            nn.Linear(2048, 1024),
             nn.ReLU(),
-            nn.Linear(512, 256),
-            nn.ReLU()
+            nn.Linear(1024, n_emb)
         )
 
         self.save_hyperparameters()
 
     def configure_optimizers(self):
         return optim.Adam(self.parameters(), lr=self.learning_rate)
-
+    
     def on_save_checkpoint(self, checkpoint) -> None:
-        torch.save(self.lm.state_dict(), "./e5-large-v2.pth")
+        torch.save(self.embedding_model.state_dict(), f"{self.model_name}.pth")
         return super().on_save_checkpoint(checkpoint)
 
     def train_dataloader(self):
@@ -51,13 +57,17 @@ class SentenceTransformerModel(L.LightningModule):
         return DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=4, persistent_workers=True, drop_last=True)
 
     def forward(self, inputs):
-        embeddings = self.lm.encode([f"query: {sentence}" for sentence in inputs], normalize_embeddings=True, convert_to_tensor=True, device=self.device)
+        embeddings = self.embedding_model.encode(inputs, normalize_embeddings=True, convert_to_tensor=True, device=self.device)
+        embeddings = self.seq(embeddings)
         return embeddings
+
+    def encode(self, inputs, normalize_embeddings=True, convert_to_tensor=True, device=None):
+        return self.embedding_model.encode(inputs, normalize_embeddings=normalize_embeddings, convert_to_tensor=convert_to_tensor, device=device)
 
     def training_step(self, batch):
         inputs = batch["text"]
-        embeddings = self.mlp(self.forward(inputs))
-        embeddings_plus = self.mlp(self.forward(inputs))
+        embeddings = self.forward(inputs)
+        embeddings_plus = self.forward(inputs)
         loss = torch.tensor(0, device=self.device, dtype=torch.float32, requires_grad=True)
         for i in range(self.batch_size):
             denominator = torch.tensor(0, device=self.device, dtype=torch.float32, requires_grad=True)
@@ -69,8 +79,8 @@ class SentenceTransformerModel(L.LightningModule):
 
     def validation_step(self, batch):
         inputs = batch["text"]
-        embeddings = self.mlp(self.forward(inputs))
-        embeddings_plus = self.mlp(self.forward(inputs))
+        embeddings = self.forward(inputs)
+        embeddings_plus = self.forward(inputs)
         loss = torch.tensor(0, device=self.device, dtype=torch.float32, requires_grad=True)
         for i in range(self.batch_size):
             denominator = torch.tensor(0, device=self.device, dtype=torch.float32, requires_grad=True)
@@ -82,6 +92,6 @@ class SentenceTransformerModel(L.LightningModule):
 
 
 if __name__ == "__main__":
-    model = SentenceTransformerModel(batch_size=64, n_ctx=512, n_emb=1024, learning_rate=1e-5, temperature=0.03)
+    model = SentenceTransformerModel(batch_size=64, n_ctx=512, n_emb=1024, learning_rate=1e-5, temperature=0.05)
     trainer = L.Trainer(max_epochs=1, default_root_dir=os.getcwd(), log_every_n_steps=5)
     trainer.fit(model)
